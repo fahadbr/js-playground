0415383d3b63d0c621d0428baddf4c865fdf88b3
Fahad Riaz <fahadbr@gmail.com>
2020-12-31 17:02:34 -0500

    wtf stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 function arraysEqStrings() {
+	console.log('arraysEqStrings')
 	let a = [1,2,3]
 	let b = [1,2,3]
 	let c = '1,2,3'
@@ -11,10 +12,37 @@ function arraysEqStrings() {
 }
 
 function appendingStringToNull() {
+	console.log('appendingStringToNull')
 	let t = null
 	//t += 'test' does not compile
 	console.log(t)
 }
 
+function arrayEquality() {
+	console.log('arrayEquality')
+	console.log("['a', 'b'] !== ['a', 'b']", ['a', 'b'] !== ['a', 'b'])
+	console.log("['a', 'b'] != ['a', 'b']", ['a', 'b'] != ['a', 'b'])
+	console.log("['a', 'b'] == ['a', 'b']", ['a', 'b'] == ['a', 'b'])
+}
+
+function stringStuff() {
+	console.log('stringStuff')
+
+	let wtf: any = 'wtf'
+	console.log("'wtf' instanceof String", wtf instanceof String)
+	console.log("typeof 'wtf' == 'string'", typeof 'wtf' == 'string')
+	console.log("typeof String('wtf')", typeof String('wtf'))
+	console.log("String('wtf') == 'wtf'", String('wtf') == 'wtf')
+}
+
+function toStringOnNums() {
+	console.log('toStringOnNums')
+	console.log("3..toString", 3..toString())
+	// console.log("3.toString", 3.toString()) doesn't compile
+}
+
 arraysEqStrings()
 appendingStringToNull()
+arrayEquality()
+stringStuff()
+toStringOnNums()
